0d90a47eea4f27d2124e3c2acb58ff250606e9a6
Eugene-Kuzmenko <e.kuzmenko@dunice.net>
2021-12-23 20:51:32 +0300

    Enable showing console output on screen

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
-#include <stdio.h>
+#include <iostream>
 
 int main(int argc, char ** argv) {
-  printf("Hello world\n");
+  using namespace std; 
+  cout << "Hello world" << '\n';
+  cout << "Did you miss me" << '\n';
 }
