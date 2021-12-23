544701d5869cc0035ab28893a64a8ab6fbafae50
Eugene-Kuzmenko <e.kuzmenko@dunice.net>
2021-12-23 20:55:04 +0300

    Add some lines

--- src/main.cpp
+++ src/main.cpp
@@ -4,4 +4,6 @@ int main(int argc, char ** argv) {
   using namespace std; 
   cout << "Hello world" << '\n';
   cout << "Did you miss me" << '\n';
+  cout << "It's C++ code compiled to webassembly. ";
+  cout << "Do you like it?\n";
 }
